dfd2bdc860dc3babb8d31edfaaa2c2c91743f04d
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-06-01 23:27:36 +0300

    add color mode for theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import {
   SignedIn,
   SignedOut,
 } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -25,10 +27,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>
-        <ClerkProvider>{children}</ClerkProvider>
+    <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="discord-theme">
+        {children}
+        </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
