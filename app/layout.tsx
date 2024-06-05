9f549d4b0bdf79f16dcf216dccab98ad549cf5b0
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-06-05 09:01:53 +0300

    socket.io setup + socket Indicator Badge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import type { Metadata } from "next";
@@ -35,8 +36,10 @@ export default function RootLayout({
             enableSystem
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
